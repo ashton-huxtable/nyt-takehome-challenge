ec955d5b460fab22d7e12065ce8b7ddc4ca9f1d1
Ashton Huxtable <aehuxtable@gmail.com>
2021-09-15 11:45:28 -0600

    Add Details component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
+import './Details.css'
+
 
 export const Details = ({selectedArticle}) => {
 
   
 
   return(
-    <div>details</div>
+    <section>
+      <img className='details-img' src={selectedArticle.multimedia[0].url} alt={selectedArticle.multimedia[0].caption} />
+      <div>
+        <h2>{selectedArticle.title}</h2>
+        <p>{selectedArticle.byline}</p>
+      </div>
+      <p>{selectedArticle.abstract}</p>
+      <button><a href={selectedArticle.url}>Read Full Article</a></button>
+    </section>
   )
 }
